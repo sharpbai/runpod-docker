2297f516b54de8146e7e9c7515ae5f485d5867a0
sharpbai <sharpbai@gmail.com>
2023-06-01 00:29:52 +0800

    feat: torch upgrade to 2.0.1

--- torch/build.sh
+++ torch/build.sh
@@ -1,1 +1,1 @@
-docker build -t runpod-torch:2.0.0-cuda11.8.0-cudnn8-devel-ubuntu22.04 -t runpod-torch:latest .
+docker build -t runpod-torch:2.0.1-cuda11.8.0-cudnn8-devel-ubuntu22.04 -t runpod-torch:latest .
