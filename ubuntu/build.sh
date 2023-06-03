3793916cec3a62a3330929da65ce4042a21d2d87
sharpbai <sharpbai@gmail.com>
2023-06-03 12:20:21 +0800

    feat: add cuda multiple version build

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,1 +1,4 @@
-docker build -t runpod-cuda:11.8.0-cudnn8-devel-ubuntu22.04 -t runpod-cuda:latest .
+CUDA_BASE_IMG_VERSION=11.7.1-cudnn8-devel-ubuntu22.04
+docker build --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} -t runpod-cuda:${CUDA_BASE_IMG_VERSION}  .
+CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu22.04
+docker build --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} -t runpod-cuda:${CUDA_BASE_IMG_VERSION} -t runpod-cuda:latest .
