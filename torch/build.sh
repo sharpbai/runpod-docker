33403359133b6d1ca9e054b5c24d1c3e8b0b148a
sharpbai <sharpbai@gmail.com>
2023-06-03 13:37:03 +0800

    feat: update build for multiple torch

--- torch/build.sh
+++ torch/build.sh
@@ -1,1 +1,13 @@
-docker build -t runpod-torch:2.0.1-cuda11.8.0-cudnn8-devel-ubuntu22.04 -t runpod-torch:latest .
+TORCH_CUDA_VERSION=cu117
+CUDA_BASE_IMG_VERSION=11.7.1-cudnn8-devel-ubuntu22.04
+docker build \
+--build-arg TORCH_CUDA_VERSION=${TORCH_CUDA_VERSION} \
+--build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
+-t runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} .
+
+TORCH_CUDA_VERSION=cu118
+CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu22.04
+docker build \
+--build-arg TORCH_CUDA_VERSION=${TORCH_CUDA_VERSION} \
+--build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
+-t runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} -t runpod-torch:latest .
