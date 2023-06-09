6946580114a8adbc45b32bae19f5e79a48534d59
sharpbai <sharpbai@gmail.com>
2023-06-09 21:49:21 +0800

    feat: add cu120 for runpod-cuda image

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -8,14 +8,14 @@ docker build --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
 -t runpod-cuda:${CUDA_BASE_IMG_VERSION} \
 -t $(whoami)/runpod-cuda:${CUDA_BASE_IMG_VERSION} .
 
-CUDA_BASE_IMG_VERSION=11.7.1-cudnn8-devel-ubuntu22.04
+CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu22.04
 docker build --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
 -t runpod-cuda:${CUDA_BASE_IMG_VERSION} \
 -t $(whoami)/runpod-cuda:${CUDA_BASE_IMG_VERSION}  .
 
-CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu22.04
+CUDA_BASE_IMG_VERSION=12.0.1-cudnn8-devel-ubuntu22.04
 docker build --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
 -t runpod-cuda:${CUDA_BASE_IMG_VERSION} \
 -t $(whoami)/runpod-cuda:${CUDA_BASE_IMG_VERSION} \
 -t runpod-cuda:latest \
--t $(whoami)runpod-cuda:latest .
+-t $(whoami)/runpod-cuda:latest .
