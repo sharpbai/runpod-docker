59ec07bd42f21cdf496dc44d3c918fe04bad79f7
sharpbai <sharpbai@gmail.com>
2023-06-09 10:47:03 +0800

    fix: fix missing tag for build.sh

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -17,4 +17,5 @@ CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu22.04
 docker build --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
 -t runpod-cuda:${CUDA_BASE_IMG_VERSION} \
 -t $(whoami)/runpod-cuda:${CUDA_BASE_IMG_VERSION} \
--t runpod-cuda:latest .
+-t runpod-cuda:latest \
+-t $(whoami)runpod-cuda:latest .
