cf62ebb09d74fd314fc0b0b6a5c0e02469c2045c
sharpbai <sharpbai@gmail.com>
2023-06-09 11:21:13 +0800

    fix: modify run-local-bind

--- kohya_ss/run-local-bind.sh
+++ kohya_ss/run-local-bind.sh
@@ -1,9 +1,10 @@
-#WORKSPACE_VOL=runpod-basara-app-volume
+WORKSPACE_VOL=runpod-kohya_ss-volume
+docker volume create -d loopback ${WORKSPACE_VOL} -o sparse=true -o size=50g
 docker run -it --rm \
-	--runtime=nvidia --gpus all  \
-	--env-file .env \
-	-p 220:22/tcp -p 8888:8888/tcp -p 7860:7860 \
-	--storage-opt size=10G \
-	-v "$(pwd)/workspace:/workspace" \
-	runpod-basaran:0.17.2
+        --runtime=nvidia --gpus all  \
+        --env-file .env \
+        -p 220:22/tcp -p 8888:8888/tcp -p 7860:7860 \
+        --storage-opt size=10G \
+        -v ${WORKSPACE_VOL}:/workspace \
+        runpod-kohya_ss:21.5.10 
 #docker volume rm ${WORKSPACE_VOL}
