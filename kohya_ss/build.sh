d3ab24a7e52242ea3cc4b5a7bf895553725b1336
sharpbai <sharpbai@gmail.com>
2023-06-10 03:34:43 +0800

    feat: add kohya-ss H100 image

--- kohya_ss/build.sh
+++ kohya_ss/build.sh
@@ -5,8 +5,8 @@ docker build \
 -t runpod-kohya_ss:21.5.10-${CUDA_VERSION} \
 -t $(whoami)/runpod-kohya_ss:21.5.10-${CUDA_VERSION} .
 
-TORCH_BASE_IMG_VERSION=2.0.1-cuda12.0.1-cudnn8-devel-ubuntu22.04
-CUDA_VERSION=cu120
+TORCH_BASE_IMG_VERSION=2.1.0-cuda12.1.1-cudnn8-devel-ubuntu22.04
+CUDA_VERSION=cu121
 docker build \
 --build-arg TORCH_BASE_IMG_VERSION=${TORCH_BASE_IMG_VERSION} \
 -t runpod-kohya_ss:21.5.10-${CUDA_VERSION} \
--- kohya_ss/push.sh
+++ kohya_ss/push.sh
@@ -1,3 +1,3 @@
 docker push sharpbai/runpod-kohya_ss:21.5.10-cu118
-docker push sharpbai/runpod-kohya_ss:21.5.10-cu120
+docker push sharpbai/runpod-kohya_ss:21.5.10-cu121
 docker push sharpbai/runpod-kohya_ss:latest
