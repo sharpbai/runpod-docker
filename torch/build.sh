21590147b444dcf5a7d4ad1dec7f29c025c56aa1
sharpbai <sharpbai@gmail.com>
2023-06-09 11:03:01 +0800

    feat: add ubuntu 20.04 torch build

--- torch/build.sh
+++ torch/build.sh
@@ -1,16 +1,40 @@
 TORCH_CUDA_VERSION=cu117
-CUDA_BASE_IMG_VERSION=11.7.1-cudnn8-devel-ubuntu22.04
+CUDA_BASE_IMG_VERSION=11.7.1-cudnn8-devel-ubuntu20.04
+PYTHON_LIB_NAME=python3.8
 docker build \
 --build-arg TORCH_CUDA_VERSION=${TORCH_CUDA_VERSION} \
 --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
+--build-arg PYTHON_LIB_NAME=${PYTHON_LIB_NAME} \
+-t runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} \
+-t $(whoami)/runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} .
+
+TORCH_CUDA_VERSION=cu118
+CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu20.04
+PYTHON_LIB_NAME=python3.8
+docker build \
+--build-arg TORCH_CUDA_VERSION=${TORCH_CUDA_VERSION} \
+--build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
+--build-arg PYTHON_LIB_NAME=${PYTHON_LIB_NAME} \
+-t runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} \
+-t $(whoami)/runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} .
+
+TORCH_CUDA_VERSION=cu117
+CUDA_BASE_IMG_VERSION=11.7.1-cudnn8-devel-ubuntu22.04
+PYTHON_LIB_NAME=python3.10
+docker build \
+--build-arg TORCH_CUDA_VERSION=${TORCH_CUDA_VERSION} \
+--build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
+--build-arg PYTHON_LIB_NAME=${PYTHON_LIB_NAME} \
 -t runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} \
 -t $(whoami)/runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} .
 
 TORCH_CUDA_VERSION=cu118
 CUDA_BASE_IMG_VERSION=11.8.0-cudnn8-devel-ubuntu22.04
+PYTHON_LIB_NAME=python3.10
 docker build \
 --build-arg TORCH_CUDA_VERSION=${TORCH_CUDA_VERSION} \
 --build-arg CUDA_BASE_IMG_VERSION=${CUDA_BASE_IMG_VERSION} \
+--build-arg PYTHON_LIB_NAME=${PYTHON_LIB_NAME} \
 -t runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} \
 -t $(whoami)/runpod-torch:2.0.1-cuda${CUDA_BASE_IMG_VERSION} \
 -t runpod-torch:latest \
--- torch/push.sh
+++ torch/push.sh
@@ -1,3 +1,5 @@
+docker push $(whoami)/runpod-torch:2.0.1-cuda11.7.1-cudnn8-devel-ubuntu20.04
+docker push $(whoami)/runpod-torch:2.0.1-cuda11.8.0-cudnn8-devel-ubuntu20.04
 docker push $(whoami)/runpod-torch:2.0.1-cuda11.7.1-cudnn8-devel-ubuntu22.04
 docker push $(whoami)/runpod-torch:2.0.1-cuda11.8.0-cudnn8-devel-ubuntu22.04
 docker push $(whoami)/runpod-torch:latest
