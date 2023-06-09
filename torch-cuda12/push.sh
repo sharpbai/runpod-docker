06e6faf286ad6620a779e2b3b56a334664df28b6
sharpbai <sharpbai@gmail.com>
2023-06-09 21:50:06 +0800

    feat: add torch cuda120

---
+++ torch-cuda12/push.sh
@@ -0,0 +1,1 @@
+docker push $(whoami)/runpod-torch:2.0.1-cuda12.0.1-cudnn8-devel-ubuntu22.04
---
+++ torch-cuda12/run-local-rm.sh
@@ -0,0 +1,10 @@
+WORKSPACE_VOL=runpod-torch-workspace-volume
+docker volume create -d loopback ${WORKSPACE_VOL} -o sparse=true -o size=50g
+docker run -it --rm \
+	--runtime=nvidia --gpus all  \
+	--env-file .env \
+	-p 220:22/tcp -p 8888:8888/tcp \
+	--storage-opt size=10G \
+	-v ${WORKSPACE_VOL}:/workspace \
+	runpod-torch:2.0.1-cuda12.0.1-cudnn8-devel-ubuntu22.04
+docker volume rm ${WORKSPACE_VOL}
